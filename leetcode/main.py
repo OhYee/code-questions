85c5add549e91f53009c61a01dd59f8799fe4462
OhYee <oyohyee@oyohyee.com>
2020-11-23 20:50:03 +0800

    misc: local code runner(#master)

---
+++ leetcode/main.py
@@ -0,0 +1,65 @@
+#!/bin/python3
+import json
+import sys
+import os
+import subprocess
+
+PWD = os.path.dirname(os.path.abspath(__file__))
+
+
+def call(*args):
+    r = os.popen(" ".join(args))
+    text = r.read()
+    r.close()
+    return text
+
+
+py3template = '''
+from typing import *
+
+{code}
+
+if __name__ == "__main__":
+    solution = Solution()
+    tests = {tests}
+    total = len(tests)
+    passed = True
+    for idx, t in enumerate(tests):
+        res = solution.{name}(t["in"])
+        if res == t["out"]:
+            pass
+        else:
+            passed = False
+            print("\033[91mError at", idx+1, "test\033[0m")
+            print(" ", "\033[1minput\033[0m", t["in"])
+            print(" ", "\033[1mgot  \033[0m", res)
+            print(" ", "\033[1mwant \033[0m", t["out"])
+
+    if passed:
+        print("\033[92mAll %d testcases passed\033[0m" % total)
+'''
+
+
+def runPython3(_id, name, tests):
+    folder = "%s/%s" % (PWD, _id)
+    source = os.path.join(folder, "%s.py" % _id)
+    tmpFolder = os.path.join(folder, "dist")
+    tmpFile = os.path.join(tmpFolder, "%s.py" % _id)
+    with open(source) as f:
+        code = f.read()
+    if not os.path.exists(tmpFolder):
+        os.mkdir(tmpFolder)
+    with open(tmpFile, "w") as f:
+        f.write(py3template.format(code=code, name=name, tests=tests))
+
+    res = call("python3", tmpFile)
+    print(res)
+
+
+if __name__ == "__main__":
+    _id = sys.argv[1]
+    with open("./%s/problem.json" % _id) as f:
+        problem = json.loads(f.read())
+    name = problem["name"]
+    tests = problem["samples"]
+    runPython3(_id, name, tests)
