3846a560d208987c758b8152177d2168e6cd43b2
OhYee <oyohyee@oyohyee.com>
2020-11-25 23:52:35 +0800

    misc: python3 runner(#master)

--- leetcode/main.py
+++ leetcode/main.py
@@ -124,25 +124,25 @@ class CodeRunner():
         self.afterGenerate(ctx, tmpFile)
         self.check(ctx, tmpFile)
 
+
+py3fmt = '''
 from typing import *
+import json
 
 {code}
 
 if __name__ == "__main__":
     solution = Solution()
-    tests = {tests}
-    total = len(tests)
-    passed = True
-    for idx, t in enumerate(tests):
-        res = solution.{name}(t["in"])
-        if res == t["out"]:
-            pass
-        else:
-            passed = False
-            print("\033[91mError at", idx+1, "test\033[0m")
-            print(" ", "\033[1minput\033[0m", t["in"])
-            print(" ", "\033[1mgot  \033[0m", res)
-            print(" ", "\033[1mwant \033[0m", t["out"])
+    print(json.dumps({{ 
+        "out": solution.{name}(json.loads(input())) 
+    }}), end="")
+'''
+
+
+class Python3Runner(CodeRunner):
+    def __init__(self):
+        super().__init__("python3", "py", "python3", py3fmt)
+
 
     if passed:
         print("\033[92mAll %d testcases passed\033[0m" % total)
