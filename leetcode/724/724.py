f7aaedce21f3fdd7bf57ca786da3467733ff4d72
OhYee <oyohyee@oyohyee.com>
2021-01-28 22:57:11 +0800

    leetcode 724

---
+++ leetcode/724/724.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        l = 0
+        r = sum(nums)
+        for i in range(n):
+            r -= nums[i]
+            if l == r:
+                return i
+            else:
+                l += nums[i]
+        return -1
