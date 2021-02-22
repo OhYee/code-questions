c37a6ccc594daf35bff5c3e341a8c5a867bcb25d
OhYee <oyohyee@oyohyee.com>
2021-02-22 14:20:14 +0800

    leetcode 766

---
+++ leetcode/766/766.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        n = len(matrix)
+        if n == 0:
+            return True
+        m = len(matrix[0])
+        for i in range(1, n):
+            for j in range(1, m):
+                if matrix[i][j] != matrix[i-1][j-1]:
+                    return False
+        return True
